78369ec70380906e3d64273d596bbfa7936ea6cd
vicksif <158256987+vicksif@users.noreply.github.com>
2024-05-15 13:42:13 +0700

    Update tugassilinder.py

--- tugassilinder.py
+++ tugassilinder.py
@@ -3,7 +3,7 @@
 
 import streamlit as st
 import numpy as np
-import matplotlib as plt
+import matplotlib pyplot as plot
   
 x=st.slider('Pilih rentang', 0.0, 2.0, (.2, .5))
 st.write('nilai x:', x)
